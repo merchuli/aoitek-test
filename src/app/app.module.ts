4580042ac7484aa31a6d199d700c06f7a968f537
merchuli <merchu.liang@gmail.com>
2020-04-11 21:01:19 +0800

    refactor: Move station selector to the folder shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './content/pages/search/search.component';
 import { ThsrService } from './content/core/service/thsr.service';
+import { StationSelectorComponent } from './content/shared/station-selector/station-selector.component';
 import { SearchTimeTableComponent } from './content/components/search-time-table/search-time-table.component';
 import { SearchStopsComponent } from './content/components/search-stops/search-stops.component';
 import { SearchAvailableSeatComponent } from './content/components/search-available-seat/search-available-seat.component';
-import { StationSelectorComponent } from './content/components/station-selector/station-selector.component';
 
 @NgModule({
   declarations: [
