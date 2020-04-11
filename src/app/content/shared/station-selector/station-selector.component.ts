87bfba66508b6561b9900d33e2b4792f4ad19065
merchuli <merchu.liang@gmail.com>
2020-04-11 21:25:27 +0800

    refactor: Tweak the codes and add some comments

--- src/app/content/shared/station-selector/station-selector.component.ts
+++ src/app/content/shared/station-selector/station-selector.component.ts
@@ -3,16 +3,16 @@ import { FormControl } from '@angular/forms';
 import { ThsrService } from '../../core/service/thsr.service';
 
 @Component({
-  // tslint:disable-next-line:component-selector
-  selector: 'station-selector',
+  selector: 'thsr-station-selector',
   templateUrl: './station-selector.component.html',
   styleUrls: ['./station-selector.component.scss']
 })
 export class StationSelectorComponent implements OnInit {
+  /** 從外部傳入 selectId */
   @Input() selectedId;
+  /** 當 selectedId 有變化時，觸發事件 changeValue */
   @Output() changeValue: EventEmitter<any> = new EventEmitter();
-  selectedStationCtrl;
-
+  selectedStationCtrl: FormControl;
   stations = [];
 
   constructor(private thsrService: ThsrService) {
@@ -20,23 +20,31 @@ export class StationSelectorComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    /** FormControl, Angular 官方用法
+     *  Definition: Tracks the value and validation status of an individual form control.
+     */
     this.selectedStationCtrl = new FormControl(this.selectedId);
+
+    /** 初始化時綁定監聽事件(selectedId) */
     this.onChanges();
   }
 
+  /** 監聽事件(selectedId)，若改變值則發送事件出去 */
   onChanges(): void {
     this.selectedStationCtrl.valueChanges.subscribe(val => {
       this.changeValue.emit(val);
     });
   }
 
+  /** 取得高鐵所有車站資料 */
   getStations() {
+    const lang = 'Zh_tw';
+
     this.thsrService.getStations()
       .subscribe((res: any) => {
         this.stations = res.map((item) => {
           return {
-            // tslint:disable-next-line:no-string-literal
-            name: item.StationName['Zh_tw'],
+            name: item.StationName[lang],
             id: item.StationID,
           };
         });
